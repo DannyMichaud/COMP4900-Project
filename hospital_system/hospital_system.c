442dc55ceb7e0f723b2884bed337b3d43e0a1c0d
DannyMichaud <PedroKid2000@gmail.com>
2022-11-28 14:26:59 -0500

    start hospital channel server

--- hospital_system/hospital_system.c
+++ hospital_system/hospital_system.c
@@ -21,9 +21,26 @@
 
 int main(int argc, char **argv) {
 
+	name_attach_t* systemChannel = malloc(sizeof(name_attach_t));
+
+	startServer(&systemChannel);
+
+	mainLoop(&systemChannel);
+
+	free(systemChannel);
 	return 0;
 }
 
-void startServer(){
+void startServer(name_attach_t** channel){
 
+	*channel = name_attach(NULL, HOSPITAL_SERVER_NAME, 0);
+
+}
+
+void mainLoop(name_attach_t** channel){
+
+	while(1){
+		printf("Hello! \n");
+		sleep(1);
+	}
 }
--- hospital_system/hospital_system.h
+++ hospital_system/hospital_system.h
@@ -1,1 +1,5 @@
-void startServer();
+#define HOSPITAL_SERVER_NAME "hospitalSystem"
+
+void startServer(name_attach_t**);
+
+void mainLoop(name_attach_t**);
